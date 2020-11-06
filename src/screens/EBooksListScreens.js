8facdb4f6e43150e8fc9cf8b77da3c708d45f281
DarwinYRV07 <darwinventura840@gnail.com>
2020-11-05 19:43:35 -0600

    Primeros cambios del Diseño

--- src/screens/EBooksListScreens.js
+++ src/screens/EBooksListScreens.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import {StyleSheet,Text, Image, FlatList } from 'react-native';
-import {Input,Container,Item, H1, Header, Button, Icon, View, Spinner, Card,CardItem, Body} from "native-base";
+import {Input,Container,Item, H1, Header,View, Spinner, Card,CardItem, Body, Button, Icon, H2} from "native-base";
 import backend from "../api/backend";
 import getEnvVars from "../../enviroment";
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const {apiCoverUrl,apiCoverSize} = getEnvVars;
 
@@ -10,7 +11,7 @@ function EBooksListScreens() {
 
 
     //Control de los estados de los libros
-    const [books, setBooks] = useState([]);
+    const [books, setBooks] = useState(null);
     const [error, setError] = useState(false);
 
     const getBooks = async () => {
@@ -18,7 +19,6 @@ function EBooksListScreens() {
         try {
 
             const response = await backend.get(`get/?category=libros_programacion&criteria=most_viewed`);
-            
             setBooks(response.data);
             console.log(books);
 
@@ -44,61 +44,172 @@ function EBooksListScreens() {
 
 
     return (
-        <Container>
-            <Header style={styles.eBooksHeader} />
-            <Item>
-                <Input placeholder="Buscar" />
-                <Button style={styles.eBooksButton}>
-                    <Icon name="search" color="black" />
-                </Button>
-            </Item>
-            <Image source={require("../../assets/eBooks.png")}
-            style={styles.eBooksImagen} />
-            <H1 style={{marginTop:20}}>Libros Mas Populares en Programación</H1>
-            <FlatList      
-                ListEmptyComponent={<Text>No hay Libros disponibles!</Text>}
-                data={books}
-                key={({item}) => item.ID} 
-                renderItem={({item}) => {       
-                return(
-                <View>
-                    <Card>
-                      <CardItem>
-                          <Body>
-                            {console.log(`${apiCoverUrl}${item.cover}`)}
-                            <Image source = {{uri: `${apiCoverUrl}${item.cover}`}} style={styles.eBooksImagen}></Image>
-                            <Text>{item.title}</Text>
-                          </Body>
-                        </CardItem>
-                    </Card>
+        <Container style={styles.container}>
+            <Header style={styles.eBooksHeader} searchBar >
+                <Item style={styles.estiloBuscador}>
+                    <Input  placeholder="Buscar" />
+                    <Button style={styles.imagenLupa} icon><Icon name="search"color="whirte" /></Button>
+                </Item>
+            </Header>
+            <Image source={require("../../assets/LogoeBooks.png")}
+            style={styles.eBooksLogo} />
+            <View style={styles.centrarContenido}>
+                <View style={styles.tituloPresentacion}>
+                    <Item >
+                        <H2 style={styles.titulos}> Más Vistos</H2>
+                        <Button style={styles.iconoMostrar} ><Icon name="book"color="whirte" /></Button>
+                    </Item>  
                 </View>
-                )
-                }} 
-
-            />
-
+                
+                <View style={styles.contenidoLibro}>
+                    <FlatList
+                        ListEmptyComponent={<Text>No hay Libros disponibles!</Text>}
+                        data={books}
+                        key={({item}) => item.ID}
+                        style={styles.librosEnFila}
+                        renderItem={({item}) => {
+                        return (
+                            <Card>
+                                <CardItem>
+                                    <Body>
+                                        <Image source={{ uri : `${item.cover}`}} style={styles.eBooksImage}></Image>
+                                        <Text>{item.title}</Text>
+                                    </Body>
+                                </CardItem>
+                            </Card>
+                        )   
+                        }}
+                        keyExtractor={(items,index) => index.toString()}
+                    />
+                </View>
+                <View style={styles.tituloPresentacion1}>
+                    <Item >
+                        <H2 style={styles.titulos}>Otros Contenidos </H2>
+                        <Button style={styles.iconoMostrar1} ><Icon name="book"color="whirte" /></Button>
+                    </Item>  
+                </View>
+            </View>
         </Container>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-        flex: 1,
-        justifyContent:"center",
-        alignItems: "center",
+        top:0,
+        height: 100,
+        backgroundColor: "#fff0f0",   
     
-    },
-    eBooksImagen:{
-        width: "100%",
-        height: 120,
-        resizeMode: "center",
     },
     eBooksHeader:{
         backgroundColor:  "#835858",
     },
-    eBooksButton:{
-        backgroundColor: "black",
+    estiloBuscador:{
+        borderTopRightRadius:20,
+        borderBottomRightRadius:20,
     },
+    imagenLupa:{
+        borderBottomRightRadius:20,
+        borderBottomLeftRadius: 20,
+        borderTopRightRadius:20,
+        borderTopLeftRadius: 20,
+        backgroundColor: "#463333",
+    },
+    eBooksLogo:{
+        width: "100%",
+        height: 120,
+        resizeMode: "center",
+    },
+    tituloPresentacion:{
+        top:5,
+        right:10,
+        left:10,
+        width:"100%",
+        backgroundColor:"white",
+        borderLeftColor: "black",
+        borderTopColor:"black",
+        borderBottomColor:"black",
+        borderBottomRightRadius:20,
+        borderBottomLeftRadius: 20,
+        borderTopRightRadius:20,
+        borderTopLeftRadius: 20,
+    },
+    tituloPresentacion1:{
+        top:50,
+        right:10,
+        left:10,
+        width:"100%",
+        backgroundColor:"white",
+        borderLeftColor: "black",
+        borderTopColor:"black",
+        borderBottomColor:"black",
+        borderBottomRightRadius:20,
+        borderBottomLeftRadius: 20,
+        borderTopRightRadius:20,
+        borderTopLeftRadius: 20,
+    },
+    titulos:{
+        fontSize:18,
+        left:10,
+    },
+    iconoMostrar:{
+        borderBottomRightRadius:20,
+        borderBottomLeftRadius: 20,
+        borderTopRightRadius:20,
+        borderTopLeftRadius: 20,
+        alignContent:"flex-end",
+        backgroundColor: "#835858",
+        left:200,
+        width:"12%",
+        height:28,
+    },
+    iconoMostrar1:{
+        borderBottomRightRadius:20,
+        borderBottomLeftRadius: 20,
+        borderTopRightRadius:20,
+        borderTopLeftRadius: 20,
+        alignContent:"flex-end",
+        backgroundColor: "#835858",
+        left:150,
+        width:"12%",
+        height:28,
+    },
+    ubicacion:{
+        backgroundColor:"red",
+        borderBottomRightRadius:20,
+        borderBottomLeftRadius: 20,
+        borderTopRightRadius:20,
+        borderTopLeftRadius: 20,
+        width:"40%",
+        height:100,
+    },
+    centrarContenido:{
+        height:100,
+        width:"90%",
+    },
+    contenidoLibro:{
+        flexDirection:"row",
+        top:20,
+        left:15,
+        width:"100%",
+        backgroundColor:"#835858",
+        height:/*230*/230,
+        borderBottomRightRadius:20,
+        borderTopLeftRadius: 20,
+    },
+    eBooksImagen:{
+        width: "60%",
+        height: 70,
+        resizeMode: "center",
+    },
+    librosEnFila:{
+        flexDirection:"row",
+    },
+    ordernarporfa:{
+        backgroundColor:"red",
+        height:200,
+        width:"20%",
+    }
+    
 });
 
 
