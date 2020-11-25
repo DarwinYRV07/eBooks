e906997fedff1a0915e5a0336ef09908ae8e80bc
DarwinYRV07 <darwinventura840@gnail.com>
2020-11-24 21:07:33 -0600

    Presentacion de la app en expo

--- enviroment.js
+++ enviroment.js
@@ -6,13 +6,17 @@ const ENV = {
         url: "https://www.etnassoft.com/api/v1/",
         apiCoverUrl: "http://collection.openlibra.com.s3.amazonaws.com/covers/",
         apiCoverSize: "720x679",
-
     },
+    default:{
+        url: "https://www.etnassoft.com/api/v1/",
+    }
 };
 
 const getEnvVars = (env = Constants.manifest.releaseChannel) => {
     if(__DEV__){
         return ENV.dev;
+    }else if (env === "default"){
+        return ENV.default;
     }
 };
 
